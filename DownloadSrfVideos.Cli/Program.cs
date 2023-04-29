b4d43507b8a0e47ad576aa4a999b815ed36679b4
iodar <granzowdario@gmail.com>
2023-04-30 00:03:40 +0200

    feature: added main method, first interface

--- DownloadSrfVideos.Cli/Program.cs
+++ DownloadSrfVideos.Cli/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DownloadSrfVideos.Cli
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
--- DownloadSrfVideos.Core/Core/IDownloader.cs
+++ DownloadSrfVideos.Core/Core/IDownloader.cs
@@ -2,6 +2,7 @@ namespace DownloadSrfVideos.Core.Core
 {
     public interface IDownloader
     {
-            
+        void DownloadFile(string uri);
+        void DownloadFile(string uri, string targetPath);
     }
 }
